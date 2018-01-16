b1bb5e3a2e3923304bb3c0fc6637db989ef81683
Lusine <lus21484@gmail.com>
2018-01-16 13:38:00 +0400

    initial

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const expressValidator = require('express-validator');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+const app = express();
+  
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressValidator());
+app.use(cookieParser())
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+app.get('/', cookieTimeCheckMiddleware, (req, res) => {
+    res.render('time.pug', { time: req.cookies.time});
+});
+
+app.get('/myroute/:param', (req, res) => {
+    const param = req.params.param,
+        queryParam = req.query.param ? req.query.param : '',
+        sessionParam = req.session.param ? req.session.param : '',
+        cookieParam =  req.cookies.param ? req.cookies.param : '';
+    res.render('param.pug', { param, queryParam, sessionParam, cookieParam});
+});
+
+app.listen(3000, () => {
+    console.log('server started')
+});
+
+// Middleware functions
+function cookieTimeCheckMiddleware(req, res, next) {
+    if (!req.cookies.time) {
+        res.cookie('time', new Date().toString());
+    }
+    next();
+}
